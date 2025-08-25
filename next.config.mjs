904d7ad88e89dfd1f4a4421c06f7c3b9c367e6eb
vetri15 <64657217+vetri15@users.noreply.github.com>
2025-08-25 18:59:00 +0530

    resource url remapping

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: "/Personal-Website",
+    assetPrefix: "/Personal-Website/",
     images: { unoptimized: true },
 }
 
