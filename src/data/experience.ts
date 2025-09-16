fe8f85237273a6f6654756671e49ca56240e6ec8
vetri15 <64657217+vetri15@users.noreply.github.com>
2025-09-16 11:54:46 +0530

    experience added

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,8 +1,8 @@
 export const experience = [
     {
         title: 'Software Developer | Mar 2023 - Sep 2024',
-        subtitle: '',
+        subtitle: 'CRM & Java Spring Boot',
         description:
-            '',
+            'I am a Software Developer skilled in Java Spring Boot microservices and Microsoft Dynamics CRM, with a strong focus on building robust and scalable enterprise systems. My background includes backend development, containerization, system integrations, and automating workflows using C#, Power Automate, and REST APIs. I specialize in implementing resilience, monitoring, and analytics with technologies such as Kafka, Docker, OAuth2/JWT, and Power BI.',
     }
 ]
