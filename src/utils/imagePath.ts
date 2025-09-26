d080c33ba59c2c297cee79bf6b4c5c101d6c150b
vetri15 <64657217+vetri15@users.noreply.github.com>
2025-09-26 15:50:47 +0530

    removed proj name in url

--- src/utils/imagePath.ts
+++ src/utils/imagePath.ts
@@ -1,5 +1,5 @@
 export function appendBaseUrl(path: string): string {
    const isProd = process.env.NODE_ENV === "production";
    const isVercel = !!process.env.VERCEL;
-   return `${ (isProd && !isVercel) ? "/Personal-Website" : ""}${path}`;
+   return `${ (isProd && !isVercel) ? "" : ""}${path}`;
 }
