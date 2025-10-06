7d84bbe09c5df153817ec465c3678d2ba8430bfb
vetri15 <64657217+vetri15@users.noreply.github.com>
2025-10-06 21:47:00 +0530

    changed edu

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,6 +1,6 @@
 export const education = [
     {
-        title: 'Sri shakthi Institute of Engineering and Technology | 2022',
+        title: 'SIET | 2022',
         subtitle: 'Bachelor of Engineering in Computer Science',
         description:
             'Graduated in Computer Science with a CGPA of 8.4, gaining a strong foundation in software development and engineering principles. This education enhanced my technical skills, problem-solving abilities, and attention to detail.'
